223a219c69fbde775840a3b1b06f0980a65d90c7
Jarred Capellman <jcapellman@hotmail.com>
2015-05-15 12:53:02 -0700

    Initial work on the Windows 10 Universal Port

---
+++ Federation.Windows/GamePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+using Microsoft.Phone.Controls;
+
+using Federation.Models;
+
+namespace Federation {
+    public partial class GamePage : PhoneApplicationPage {
+        private GameModel viewModel {
+            get { return (GameModel) DataContext; }
+        }
+
+        public GamePage() {
+            InitializeComponent();
+
+            DataContext = new GameModel();
+        }
+
+        private void LBtnNextTurn_OnTap(object sender, GestureEventArgs e) { viewModel.NextTurn(); }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e) {
+            rwPause.IsOpen = true;
+            e.Cancel = true;
+        }
+    }
+}
