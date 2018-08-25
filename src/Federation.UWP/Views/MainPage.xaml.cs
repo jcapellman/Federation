c410d6983c8467353f273863ee1e6b452960fdf0
Jarred Capellman <jcapellman@hotmail.com>
2018-08-25 16:37:31 -0500

    Added: Implementing the Navigation Changing logic

--- src/Federation.UWP/Views/MainPage.xaml.cs
+++ src/Federation.UWP/Views/MainPage.xaml.cs
@@ -1,30 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using Federation.UWP.ViewModel;
+
+using NavigationView = Windows.UI.Xaml.Controls.NavigationView;
+using NavigationViewSelectionChangedEventArgs = Windows.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs;
+using NavigationViewItem = Windows.UI.Xaml.Controls.NavigationViewItem;
 
 namespace Federation.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MainPageViewModel ViewModel => (MainPageViewModel) DataContext;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void NavigationView_OnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            mainContent.Navigate(ViewModel.GetSelectedPageType((NavigationViewItem)args.SelectedItem));
         }
     }
 }
