442cc40d2a793db8a470719577fe3498da457ed9
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 11:40:35 -0500

    Change: Swapped out the hard coded strings for a localized page title

--- src/Federation.UWP/ViewModel/MainPageViewModel.cs
+++ src/Federation.UWP/ViewModel/MainPageViewModel.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
+using Federation.UWP.Enums;
 using Federation.UWP.Objects.UI;
 using Federation.UWP.Views;
-
-using NavigationViewItem = Windows.UI.Xaml.Controls.NavigationViewItem;
 
 namespace Federation.UWP.ViewModel
 {
@@ -12,13 +11,13 @@ namespace Federation.UWP.ViewModel
     {
         private readonly IList<NavigationItem> _pages = new List<NavigationItem>
         {
-            new NavigationItem("galaxy_map", typeof(GalaxyMapPage), "Galaxy Map"),
-            new NavigationItem("territory", typeof(TerritoryPage), "Territory"),
-            new NavigationItem("shipyards", typeof(ShipyardsPage), "Shipyards"),
-            new NavigationItem("research", typeof(ResearchPage), "Research"),
-            new NavigationItem("resources", typeof(ResourcesPage), "Resources"),
-            new NavigationItem("save_game", typeof(SaveGamePage), "Save Game"),
-            new NavigationItem("quit_to_menu", typeof(MainMenuPage), "Quit to Main Menu")
+            new NavigationItem("galaxy_map", typeof(GalaxyMapPage), LocalizedStrings.MainNavigation_GalaxyMap),
+            new NavigationItem("territory", typeof(TerritoryPage), LocalizedStrings.MainNavigation_Territory),
+            new NavigationItem("shipyards", typeof(ShipyardsPage), LocalizedStrings.MainNavigation_Shipyards),
+            new NavigationItem("research", typeof(ResearchPage), LocalizedStrings.MainNavigation_Research),
+            new NavigationItem("resources", typeof(ResourcesPage), LocalizedStrings.MainNavigation_Resources),
+            new NavigationItem("save_game", typeof(SaveGamePage), LocalizedStrings.MainNavigation_SaveGame),
+            new NavigationItem("quit_to_menu", typeof(MainMenuPage), LocalizedStrings.MainNavigation_QuitToMainMenu)
         };
 
         private NavigationItem _selectedNavigationItem;
