6525ca8f738208af2b920c2fb11ac3ed0c30347e
Jarred Capellman <jcapellman@hotmail.com>
2018-08-25 20:31:49 -0500

    Added: NavigationItem Class

---
+++ src/Federation.UWP/Objects/UI/NavigationItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Federation.UWP.Objects.UI
+{
+    public class NavigationItem
+    {
+        public Type PageType { get; set; }
+
+        public string Tag { get; set; }
+
+        public string PageTitle { get; set; }
+
+        public NavigationItem() { }
+
+        public NavigationItem(string tag, Type pageType, string pageTitle)
+        {
+            PageTitle = pageTitle;
+            Tag = tag;
+            PageType = pageType;
+        }
+    }
+}
