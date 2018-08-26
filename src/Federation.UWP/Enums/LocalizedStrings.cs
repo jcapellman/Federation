5909b5d20eb8b7d3217e8156e17c775218c635be
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 11:39:53 -0500

    Added: LocalizedStrings Enum

---
+++ src/Federation.UWP/Enums/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿namespace Federation.UWP.Enums
+{
+    public enum LocalizedStrings
+    {
+        MainNavigation_NextTurn,
+        MainNavigation_GalaxyMap,
+        MainNavigation_QuitToMainMenu,
+        MainNavigation_Research,
+        MainNavigation_Resources,
+        MainNavigation_SaveGame,
+        MainNavigation_Shipyards,
+        MainNavigation_Territory
+    }
+}
