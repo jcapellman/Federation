a0ff5cff55dd1f0c9dfe838fc9acfd16de027d9a
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 11:39:40 -0500

    Added: Enum to Localized String Extension Method

---
+++ src/Federation.UWP/Common/ExtensionMethods.cs
@@ -0,0 +1,9 @@
+﻿using Federation.UWP.Enums;
+
+namespace Federation.UWP.Common
+{
+    public static class ExtensionMethods
+    {
+        public static string ToLocalizedString(this LocalizedStrings name) => Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView().GetString(name.ToString());
+    }
+}
