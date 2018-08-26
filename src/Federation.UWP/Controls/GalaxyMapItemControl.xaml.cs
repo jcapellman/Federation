a7f211c545a8a99723d8d65f92cdd3c0996cdc4a
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 11:24:52 -0500

    Added: Initial work on the GalaxyMapItemControl

---
+++ src/Federation.UWP/Controls/GalaxyMapItemControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+using Federation.UWP.Enums;
+
+namespace Federation.UWP.Controls
+{
+    public sealed partial class GalaxyMapItemControl : UserControl
+    {
+        public Race SelectedRace
+        {
+            get => (Race) GetValue(RaceProperty);
+            set
+            {
+                SetValue(RaceProperty, value);
+
+                imgRace.Source = new BitmapImage(new Uri($"ms-appx:///Assets/Races/{value}.png"));
+            }
+        }
+
+        public static readonly DependencyProperty RaceProperty = DependencyProperty.Register("SelectedRace", typeof(Race), typeof(GalaxyMapItemControl), new PropertyMetadata(Race.Federation));
+
+        public GalaxyMapItemControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
