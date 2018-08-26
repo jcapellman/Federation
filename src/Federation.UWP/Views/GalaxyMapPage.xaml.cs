5a6b33b7bf9ec38aded100ac4185d1a9a905cf1f
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 09:16:13 -0500

    Added: Initial work on the Galaxy Map UI

--- src/Federation.UWP/Views/GalaxyMapPage.xaml.cs
+++ src/Federation.UWP/Views/GalaxyMapPage.xaml.cs
@@ -1,30 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Federation.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class GalaxyMapPage : Page
     {
         public GalaxyMapPage()
         {
             this.InitializeComponent();
+
+            var federationImage = new BitmapImage(new Uri("ms-appx:///Assets/Races/Federation.png"));
+
+            for (var x = 0; x < 5; x++)
+            {
+                for (var y = 0; y < 5; y++)
+                {
+                    ugMap.Children.Add(new Image
+                    {
+                        Source = federationImage
+                    });
+                }
+            }
         }
     }
 }
