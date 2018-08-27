7d4689334998c3be82678f277a9cff739b843329
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 20:21:25 -0500

    Added: Initial work on the MainGameItem class

---
+++ src/Federation.UWP/Objects/Game/MainGameItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using Federation.UWP.Enums;
+
+namespace Federation.UWP.Objects.Game
+{
+    public class MainGameItem
+    {
+        public GameSize GameSize { get; set; }
+
+        public List<GalaxyMapItem> GalaxyMapItems { get; set; }
+
+        public MainGameItem(GameSize gameSize)
+        {
+            GalaxyMapItems = new List<GalaxyMapItem>();
+
+            for (var x = 0; x < (int) gameSize; x++)
+            {
+                GalaxyMapItems.Add(new GalaxyMapItem
+                {
+                    Race = Race.Federation,
+                    SystemName = "SOL"
+                });
+            }
+        }
+    }
+}
