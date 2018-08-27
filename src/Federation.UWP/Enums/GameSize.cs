67bdd0d81d98cf8cf8e532e4c5b93c13b433e127
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 20:14:55 -0500

    Added: GameSize Enumeration

---
+++ src/Federation.UWP/Enums/GameSize.cs
@@ -0,0 +1,11 @@
+﻿namespace Federation.UWP.Enums
+{
+    public enum GameSize
+    {
+        TINY = 9,
+        SMALL = 12,
+        MEDIUM = 15,
+        LARGE = 21,
+        EXTRA_LARGE = 29
+    }
+}
