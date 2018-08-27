051dcd76781cf04479554ce00be0c6da9806f742
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 20:18:27 -0500

    Added: MainMenuPage game initialization

--- src/Federation.UWP/Views/MainMenuPage.xaml.cs
+++ src/Federation.UWP/Views/MainMenuPage.xaml.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using Federation.UWP.Enums;
+using Federation.UWP.Objects.Game;
 
 namespace Federation.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainMenuPage : Page
     {
         public MainMenuPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            Loaded += MainMenuPage_Loaded;
+        }
+
+        private void MainMenuPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var game = new MainGameItem(GameSize.TINY);
+            
+            Frame.Navigate(typeof(MainPage), game);
         }
     }
 }
