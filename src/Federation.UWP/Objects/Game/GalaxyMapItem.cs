fab7c9787cabd723bfe950ce787d6a28d4504bcc
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 20:21:02 -0500

    Added: Initial work on the GalaxyMapItem class

---
+++ src/Federation.UWP/Objects/Game/GalaxyMapItem.cs
@@ -0,0 +1,11 @@
+﻿using Federation.UWP.Enums;
+
+namespace Federation.UWP.Objects.Game
+{
+    public class GalaxyMapItem
+    {
+        public Race Race { get; set; }
+
+        public string SystemName { get; set; }
+    }
+}
