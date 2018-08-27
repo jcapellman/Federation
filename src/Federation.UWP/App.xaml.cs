225c9ddc181e3dd30f35a32b1ef7dfe69094a737
Jarred Capellman <jcapellman@hotmail.com>
2018-08-26 20:15:51 -0500

    Change: Navigation to MainMenuPage instead of MainPage

--- src/Federation.UWP/App.xaml.cs
+++ src/Federation.UWP/App.xaml.cs
@@ -59,7 +59,7 @@ namespace Federation.UWP
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(MainMenuPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
