6bdabf1795e4afa90c6e21f8491b55f2945d8b89
Jarred Capellman <jcapellman@hotmail.com>
2018-08-28 20:38:11 -0500

    Added: Initial Constants class

---
+++ src/Federation.UWP/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Federation.UWP.Common
+{
+    public class Constants
+    {
+        public const string FEDERATION_GAMESTART_SYSTEM = "SOL";
+
+        public const string GAMESTART_UNKNOWN_SYSTEM = "Unknown";
+    }
+}
