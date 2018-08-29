8157fb5aed573daa4ccaa96ea0a7a0a342ae88c3
Jarred Capellman <jcapellman@hotmail.com>
2018-08-28 21:20:33 -0500

    Added: Initial work on the MainMenuPageViewModel class

---
+++ src/Federation.UWP/ViewModel/MainMenuPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+using Windows.UI.Xaml.Controls;
+
+using Federation.UWP.Commands;
+using Federation.UWP.Enums;
+using Federation.UWP.Objects.Game;
+using Federation.UWP.Views;
+
+namespace Federation.UWP.ViewModel
+{
+    public class MainMenuPageViewModel : BaseViewModel
+    {
+        public ICommand NewGameCommand;
+
+        public MainMenuPageViewModel()
+        {
+            NewGameCommand = new DelegateCommand(NewGame);
+        }
+
+        public void NewGame(object param)
+        {
+            var game = new MainGameItem(GameSize.TINY);
+
+            ((Page)param).Frame.Navigate(typeof(MainMenuPage), game);
+        }
+    }
+}
