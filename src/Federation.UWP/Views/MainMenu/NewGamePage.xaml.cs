4d5343efe4f4efcda490e1b81487f53bfcc795d0
Jarred Capellman <jcapellman@hotmail.com>
2018-08-31 14:54:31 -0500

    Added: Responsive View for the Race Selection

--- src/Federation.UWP/Views/MainMenu/NewGamePage.xaml.cs
+++ src/Federation.UWP/Views/MainMenu/NewGamePage.xaml.cs
@@ -1,17 +1,35 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
+using Federation.UWP.Controls;
+using Federation.UWP.Common;
 using Federation.UWP.ViewModel.Menu;
 
 namespace Federation.UWP.Views.MainMenu
 {
     public sealed partial class NewGamePage : Page
     {
+        private NewGameViewModel ViewModel => (NewGameViewModel) DataContext;
+
         public NewGamePage()
         {
             this.InitializeComponent();
 
             DataContext = new NewGameViewModel();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ugRaces.Children.Clear();
+
+            foreach (var race in ExtensionMethods.RaceList)
+            {
+                ugRaces.Children.Add(new RaceSelectionItemControl
+                {
+                    SelectedRace = race
+                });
+            }
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
