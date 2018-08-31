95ae0e710512087867a7d30b5c6e5aa43687b404
Jarred Capellman <jcapellman@hotmail.com>
2018-08-31 14:53:38 -0500

    Added: RaceList static method to enumerate the available Races

--- src/Federation.UWP/Common/ExtensionMethods.cs
+++ src/Federation.UWP/Common/ExtensionMethods.cs
@@ -1,9 +1,15 @@
-﻿using Federation.UWP.Enums;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Federation.UWP.Enums;
 
 namespace Federation.UWP.Common
 {
     public static class ExtensionMethods
     {
         public static string ToLocalizedString(this LocalizedStrings name) => Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView().GetString(name.ToString());
+
+        public static IEnumerable<Race> RaceList => Enum.GetNames(typeof(Race)).Where(a => a != Race.Unknown.ToString()).Select(a => (Race)Enum.Parse(typeof(Race), a));
     }
 }
