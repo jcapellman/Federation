f1f724b9fa4ddedebd93bd019fe3c56ae1e0ff63
Jarred Capellman <jcapellman@hotmail.com>
2018-09-01 09:10:26 -0500

    Added: Initial work on building the GameItem object Change: Renamed the MapSize to GameSize for consistentcy

--- src/Federation.UWP/ViewModel/Menu/NewGameViewModel.cs
+++ src/Federation.UWP/ViewModel/Menu/NewGameViewModel.cs
@@ -3,23 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Federation.UWP.Enums;
+using Federation.UWP.Objects.Game;
 
 namespace Federation.UWP.ViewModel.Menu
 {
     public class NewGameViewModel : BaseViewModel
     {
-        private List<string> _mapSizeOptions;
+        private List<string> _gameSizeOptions;
 
-        public List<string> MapSizeOptions
+        public List<string> GameSizeOptions
         {
-            get => _mapSizeOptions;
+            get => _gameSizeOptions;
 
-            set { _mapSizeOptions = value; OnPropertyChanged(); }
+            set { _gameSizeOptions = value; OnPropertyChanged(); }
+        }
+
+        private string _selectedGameSize;
+
+        public string SelectedGameSize
+        {
+            get => _selectedGameSize;
+
+            set { _selectedGameSize = value; OnPropertyChanged(); }
         }
 
         public NewGameViewModel()
         {
-            MapSizeOptions = Enum.GetNames(typeof(Enums.GameSize)).ToList();
+            GameSizeOptions = Enum.GetNames(typeof(Enums.GameSize)).ToList();
         }
 
         private Race _selectedRace;
@@ -27,6 +37,15 @@ namespace Federation.UWP.ViewModel.Menu
         public void SetRace(Race race)
         {
             _selectedRace = race;
+        }
+
+        public MainGameItem BuildGameObject()
+        {
+            var gameItem = new MainGameItem((GameSize)Enum.Parse(typeof(GameSize), SelectedGameSize));
+
+            // TODO Set Selected Race here
+
+            return gameItem;
         }
     }
 }
