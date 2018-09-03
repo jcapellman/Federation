f21707a94c36971fde2fbfb42f98dcf423290975
Jarred Capellman <jcapellman@hotmail.com>
2018-09-03 18:48:36 -0500

    Added: Initial work on the BaseDAL class

---
+++ src/Federation.UWP/DAL/Base/BaseDAL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using Federation.UWP.Objects.Game;
+
+namespace Federation.UWP.DAL.Base
+{
+    public abstract class BaseDAL
+    {
+        public abstract IEnumerable<MainGameItem> GetGameList();
+
+        public abstract bool SaveGame(MainGameItem gameItem);
+    }
+}
