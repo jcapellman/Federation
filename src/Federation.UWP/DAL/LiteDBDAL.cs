ca2b4abcc0e20c5158a7b60ea4aa4538d68463ab
Jarred Capellman <jcapellman@hotmail.com>
2018-09-03 18:52:04 -0500

    Added: Initial work on the LiteDBDAL class

---
+++ src/Federation.UWP/DAL/LiteDBDAL.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Windows.Storage;
+
+using Federation.UWP.DAL.Base;
+using Federation.UWP.Objects.Game;
+
+using LiteDB;
+
+namespace Federation.UWP.DAL
+{
+    public class LiteDBDAL : BaseDAL
+    {
+        private const string DbFileName = "Federation.db";
+
+        private static string ConnectionString => Path.Combine(ApplicationData.Current.LocalFolder.Path, DbFileName);
+
+        public override IEnumerable<MainGameItem> GetGameList()
+        {
+            using (var db = new LiteDatabase(ConnectionString))
+            {
+                return db.GetCollection<MainGameItem>().FindAll();
+            }
+        }
+
+        public override bool SaveGame(MainGameItem gameItem)
+        {
+            using (var db = new LiteDatabase(ConnectionString))
+            {
+                var gameCollection = db.GetCollection<MainGameItem>();
+
+                gameCollection.Insert(gameItem);
+            }
+
+            return true;
+        }
+    }
+}
