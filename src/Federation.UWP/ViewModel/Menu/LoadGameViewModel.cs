ba40af6d55f69cf6ccf79d1083d075e041a2b634
Jarred Capellman <jcapellman@hotmail.com>
2018-09-03 19:11:50 -0500

    Added: Initial work on the LoadGameViewModel

---
+++ src/Federation.UWP/ViewModel/Menu/LoadGameViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+using Federation.UWP.DAL;
+using Federation.UWP.Objects.Game;
+
+namespace Federation.UWP.ViewModel.Menu
+{
+    public class LoadGameViewModel : BaseViewModel
+    {
+        private ObservableCollection<MainGameItem> _gameItems;
+
+        public ObservableCollection<MainGameItem> GameItems
+        {
+            get => _gameItems;
+
+            set
+            {
+                _gameItems = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public LoadGameViewModel()
+        {
+            GameItems = new ObservableCollection<MainGameItem>(new LiteDBDAL().GetGameList());
+        }
+    }
+}
