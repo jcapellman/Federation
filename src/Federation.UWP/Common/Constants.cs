b1a62338511de6031d23c2cee523705d3dca87f4
Jarred Capellman <jcapellman@hotmail.com>
2018-09-07 20:35:35 -0500

    Added: Default values in the Constants class

--- src/Federation.UWP/Common/Constants.cs
+++ src/Federation.UWP/Common/Constants.cs
@@ -5,5 +5,11 @@
         public const string FEDERATION_GAMESTART_SYSTEM = "SOL";
 
         public const string GAMESTART_UNKNOWN_SYSTEM = "Unknown";
+
+        public const int DEFAULT_NEW_GAME_CREDITS = 1000;
+
+        public const int DEFAULT_NEW_GAME_DILITHIUM = 100;
+
+        public const int DEFAULT_NEW_GAME_RESEARCH_LEVEL = 1;
     }
 }
