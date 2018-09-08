fc7c563ea71a73547afb6349c38aae8321f6b769
Jarred Capellman <jcapellman@hotmail.com>
2018-09-07 20:37:11 -0500

    Added: Constructor initialization to the default values in the FactionStateGameItem

--- src/Federation.UWP/Objects/Game/FactionStateGameItem.cs
+++ src/Federation.UWP/Objects/Game/FactionStateGameItem.cs
@@ -1,4 +1,5 @@
-﻿using Federation.UWP.Enums;
+﻿using Federation.UWP.Common;
+using Federation.UWP.Enums;
 
 namespace Federation.UWP.Objects.Game
 {
@@ -11,5 +12,13 @@ namespace Federation.UWP.Objects.Game
         public int Credits { get; set; }
 
         public int ResearchLevel { get; set; }
+        
+        public FactionStateGameItem(Faction faction)
+        {
+            Faction = Faction;
+            Credits = Constants.DEFAULT_NEW_GAME_CREDITS;
+            DilithiumCrystals = Constants.DEFAULT_NEW_GAME_DILITHIUM;
+            ResearchLevel = Constants.DEFAULT_NEW_GAME_RESEARCH_LEVEL;
+        }
     }
 }
